a81e922312d4f38ee22469ab008459e99cb1423a
nathszpil <nathanszpilman@hotmail.com>
2024-04-09 19:04:09 +0300

    created 2 gradio apps for two different tasks, pixel art creation, and model prediction

--- covers_to_art.py
+++ covers_to_art.py
@@ -1,16 +1,23 @@
 from colors import *
 from images import *
+import gradio as gr
 
-image_target =
+image_target = ""
 threshold = 70
 
-current_directory = os.getcwd()
-resize_images(load_images_in_directory("colors_covers"), "resized_images", 102, 102)
 
-covers = load_images_in_directory('resized_images')
-image = Image.open(image_target)
-colors = image.getcolors()
-images_by_color = get_images_by_colors(covers, colors, threshold)
-images = get_images_by_pixel(images_by_color, image, colors)
-combined_image = combine_images(images, image)
-combined_image.save(current_directory + "/output_" + image_target)
+def covers_to_art(folder, target_image, threshold=70):
+    threshold = int(threshold)
+    resize_images(load_images_in_directory(folder), "resized_images", 50, 50)
+    covers = load_images_in_directory('resized_images')
+    image = Image.open(target_image)
+    colors = image.getcolors()
+    images_by_color = get_images_by_colors(covers, colors, threshold)
+    images = get_images_by_pixel(images_by_color, image, colors)
+    combined_image = combine_images(images, image)
+    return combined_image
+
+
+demo = gr.Interface(fn=covers_to_art, inputs=["text", "text", "text"], outputs="image", title="Musaic art creator",
+                    description="Upload a vinyl/cd picture to detect the album and print some info about it.")
+demo.launch()
