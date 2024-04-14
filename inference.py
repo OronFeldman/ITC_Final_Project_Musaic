4bff628107f035b9d34337eb8fa6b15be5a548bc
Oron Feldman <oronfeldman@gamil.com>
2024-04-14 15:15:55 +0300

    Creating inference.py file with a python function for inferencing from model.

---
+++ inference.py
@@ -0,0 +1,12 @@
+import cv2
+from tensorflow.keras.models import load_model
+import numpy as np
+
+
+def inference_func(img, classes_dict, names_dict):
+    model = load_model('InceptionResNetV2_model')
+    predictions = model.predict(img)
+    predicted_label = classes_dict[str(np.argmax(predictions, axis=1)[0])]
+    album_name = names_dict[str(predicted_label)]
+    print(f'The predicted label is: {predicted_label}')
+    print(f'The album name is: {album_name}')
